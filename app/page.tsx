dfe889bf0343dda7d918935dc18b933a9e034082
Samuel <101070043+cyoo934@users.noreply.github.com>
2025-01-30 16:11:05 +1300

    Final Final TESTING: Completed force deployment on Fargate. Try to minimise deployment on main from now on -> Branch when required

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,11 @@ export default function Home() {
         <h1 className="text-4xl font-bold">Align</h1>
         <p className="text-lg text-gray-500">A revoluationary tool for improving consultation efficiency</p>
         <p className="text-lg text-gray-500">and patient satisfaction</p>
+
+        <div className="mt-10 flex gap-2 justify-between">
+          <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-300">Get Started</button>
+          <button className="px-4 py-2 bg-gray-200 text-gray-800 rounded-md hover:bg-gray-300 transition duration-300">Learn More</button>
+        </div>
       </div>
     </div>
   );
