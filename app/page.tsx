c2e05a2bd9543af968e04b77044fd14311e1dc02
Samuel <101070043+cyoo934@users.noreply.github.com>
2025-01-30 14:06:16 +1300

    Test 2 for CI/CD - Changed next.js website content

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
       <div className="flex flex-col items-center">
         <h1 className="text-4xl font-bold">Align</h1>
         <p className="text-lg text-gray-500">A revoluationary tool for improving consultation efficiency</p>
+        <p className="text-lg text-gray-500">and patient satisfaction</p>
       </div>
     </div>
   );
