e771fa6e9657a53d9dd943b96ed7e24c810df797
Samuel <101070043+cyoo934@users.noreply.github.com>
2025-01-30 17:11:34 +1300

    Increased complexity of AWS Architecture by adding DynamoDB Table, S3 Bucket and Cognito

--- cdk-align-web/src/main/java/com/myorg/CdkAlignWebStack.java
+++ cdk-align-web/src/main/java/com/myorg/CdkAlignWebStack.java
@@ -1,17 +1,77 @@
 package com.myorg;
 
 import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.services.cognito.PasswordPolicy;
+import software.amazon.awscdk.services.cognito.StandardAttribute;
+import software.amazon.awscdk.services.cognito.StandardAttributes;
+import software.amazon.awscdk.services.cognito.UserPool;
+import software.amazon.awscdk.services.dynamodb.Attribute;
+import software.amazon.awscdk.services.dynamodb.AttributeType;
+import software.amazon.awscdk.services.dynamodb.BillingMode;
+import software.amazon.awscdk.services.dynamodb.Table;
+import software.amazon.awscdk.services.dynamodb.TableEncryption;
 import software.amazon.awscdk.services.ec2.Vpc;
 import software.amazon.awscdk.services.ecr.IRepository;
 import software.amazon.awscdk.services.ecr.Repository;
 import software.amazon.awscdk.services.ecs.*;
 import software.amazon.awscdk.services.ecs.patterns.*;
+import software.amazon.awscdk.services.s3.BlockPublicAccess;
+import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.s3.LifecycleRule;
+import software.amazon.awscdk.services.s3.StorageClass;
+import software.amazon.awscdk.services.s3.Transition;
 import software.constructs.Construct;
+
+import java.util.List;
+
+import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.Environment;
 
 public class CdkAlignWebStack extends Stack {
     public CdkAlignWebStack(final Construct scope, final String id) {
         super(scope, id);
+
+        // Create a DynamoDB Table for storing patient data
+        Table dynamoDBTable = Table.Builder.create(this, "AlignDynamoDBTable")
+                .tableName("align-patient-data")
+                .billingMode(BillingMode.PAY_PER_REQUEST)
+                .partitionKey(Attribute.builder()
+                        .name("PatientID")
+                        .type(AttributeType.STRING)
+                        .build())
+                .encryption(TableEncryption.AWS_MANAGED)
+                .pointInTimeRecovery(true)
+                .build();
+
+        // Create an S3 Bucket for storing chat data
+        Bucket s3Bucket = Bucket.Builder.create(this, "AlignS3Bucket")
+                .bucketName("align-chat-data")
+                .versioned(true)
+                .lifecycleRules(List.of(LifecycleRule.builder()
+                        .id("ArchiveOldData")
+                        .transitions(List.of(Transition.builder()
+                                .transitionAfter(Duration.days(90))
+                                .storageClass(StorageClass.GLACIER)
+                                .build()))
+                        .build()))
+                .blockPublicAccess(BlockPublicAccess.BLOCK_ALL)
+                .build();
+
+        // Cognito User Pool for strong user authentication
+        UserPool cognitoUserPool = UserPool.Builder.create(this, "AlignCognitoUserPool")
+                .userPoolName("AlignUserPool")
+                .passwordPolicy(PasswordPolicy.builder()
+                        .minLength(12)
+                        .requireLowercase(true)
+                        .requireUppercase(true)
+                        .requireDigits(true)
+                        .requireSymbols(true)
+                        .build())
+                .standardAttributes(StandardAttributes.builder()
+                        .email(StandardAttribute.builder().required(true).mutable(false).build())
+                        .preferredUsername(StandardAttribute.builder().required(false).mutable(true).build())
+                        .build())
+                .build();
 
         // Create a minimal-cost VPC (2) AZ)
         Vpc vpc = Vpc.Builder.create(this, "AlignWebVpc")
@@ -30,7 +90,7 @@ public class CdkAlignWebStack extends Stack {
         // Create a Fargate Service with ALB
         ApplicationLoadBalancedFargateService fargateService =
                 ApplicationLoadBalancedFargateService.Builder.create(this, "AlignWebFargateService")
-                        .serviceName("AlignWebFargateService")
+                        .serviceName("AlignWebFargateService")  
                         .cluster(cluster)
                         .cpu(256) // Minimal cost (0.25 vCPU)
                         .memoryLimitMiB(512) // Minimal memory
