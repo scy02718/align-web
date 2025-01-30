09eeb48b6d740c0f1390bec7031bb52a96902e10
Samuel <101070043+cyoo934@users.noreply.github.com>
2025-01-30 15:47:09 +1300

    Final test! Force new deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="w-full h-full flex items-center justify-center bg-gray-100">
-      <div className="flex flex-col items-center">
+    <div className="w-full h-full flex items-center justify-center">
+      <div className="flex flex-col items-center p-8 bg-white rounded-lg shadow-lg mt-10 hover:shadow-2xl transition duration-300">
         <h1 className="text-4xl font-bold">Align</h1>
         <p className="text-lg text-gray-500">A revoluationary tool for improving consultation efficiency</p>
         <p className="text-lg text-gray-500">and patient satisfaction</p>
